dfff4105a3688c563890682c5426adf469b6361e
llWaadAlotaibi <waadsaud778@gmail.com>
2025-05-12 15:13:32 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+var today = new Date();
+alert("Today is " + today.toString());
+
+function highlightSkills() {
+    var section = document.getElementById("skills");
+    section.style.backgroundColor = "#FFFFCC";
+    section.style.border = "2px solid #999";
+}
